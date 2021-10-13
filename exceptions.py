67ce7883561953c7a59bec82743210cff684bd7f
nosoyyo <oyyoson@gmail.com>
2021-10-13 18:43:59 +0800

    init

---
+++ exceptions.py
@@ -0,0 +1,6 @@
+
+class AlreadyFloppedError(Exception):
+    pass
+
+class OverBetError(Exception):
+    pass
---
+++ utils.py
@@ -0,0 +1,4 @@
+num_list = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+suit_list = ['s', 'h', 'c', 'd']
+RawCards = [x + y for x in num_list for y in suit_list] 
+
