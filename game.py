9b07c731d0b39236dd26ae4ac9b56b689a145e3b
nosoyyo <oyyoson@gmail.com>
2021-10-14 17:19:13 +0800

    change structure, add Game

--- game.py
+++ game.py
@@ -7,8 +7,6 @@ from typing import List
 
 from thpoker.core import Hand, Table, Combo, Cards
 #from exceptions import *
-#from utils import DEBUG, Shuffle, CORPUS
-
 
 
 class Pool:
@@ -26,17 +24,17 @@ class Pool:
     def ShowPool(self, name, bet)-> None:
         print(f'{name}下注{bet}')
         if len(self.pools) == 1:
-            print(f'目前底池：{self.pools[0]}')
+            print(f'目前底池：{self.pools[0]}\n')
 
 
 class Player():
-    def __init__(self, name=None, AI=True) -> None:
+    def __init__(self, cash, name=None, AI=True) -> None:
         if not name:
             name = 'arslan'
         self.name = name
         self.AI = AI
         self.hand = []
-        self.cash = 600
+        self.cash = cash
 
     def __repr__(self) -> str:
         return f'<玩家：{self.name}>'
@@ -47,7 +45,7 @@ class Player():
         self.hand.append(RawCards.pop())
         self.Combo()
         if DEBUG:
-            print(f'{self.name}的手牌：{self.ShowHand()}')
+            print(f'{self.name}的手牌：{self.ShowHand()}\n')
     
     def ShowHand(self) -> Hand:
         string = ''
@@ -93,17 +91,25 @@ class Player():
         # ace check
         return 50
     
+    def ChooseOpponent(self):
+        opponent = random.choice(PLAYERS)
+        if opponent.name == self.name:
+            opponent = self.ChooseOpponent()
+        return opponent
+
     def Decide(self):
+        global LASTBET
+
+        print(f'{self.name}正在决策...')
         time.sleep(random.random()+2)
-        if DEBUG:
-            print(f'{self.name}正在决策...')
+
         # power = self.PowerCheck() #TODO
         power = int(random.random() * 100)
         if power < 20:
             self.Talk('fold')
         elif 20 <= power < 50:
             self.Talk('call')
-            self.Bet(SB)
+            bet = LASTBET
         elif 50 <= power < 90:
             self.Talk('raise')
             bet  = SB * (int(random.random()*10) + 1)
@@ -111,43 +117,121 @@ class Player():
         elif power >= 90:
             self.Talk('allin')
             bet = self.Bet(self.cash)
+        self.Bet(SB)
 
 
-def ChooseOpponent(p):
-    opponent = random.choice(PLAYERS)
-    if opponent.name == p.name:
-        opponent = ChooseOpponent(p)
-    return opponent
+class Game():
+    
+    def __init__(self, n_AI=5, SB=5, cash=600):
+        self.DEBUG = True
+        self.TABLE = []
+        self.PREFLOP_FLAG = False
+        self.FLOP_FLAG = False
+        self.TURN_FLAG = False
+        self.RIVER_FLAG = False
+        self.POOL = Pool()
+        self.SB = SB
+        self.BB = SB*2
+        self.LASTBET = self.BB
+        self.PLAYER = Player(cash=cash, AI=False)
+        self.RawCards = self.Shuffle()
 
-def MakeUpPlayers(num: int=5):
-    '''
-    :TODO: randomly decide who is SB/BB/...
-    0 - SB
-    1 - BB
-    2 - UTG
-    3 - UTG + 1
-    4 - CO
-    5 - BT
-    '''
-    global PLAYERS
-    PLAYERS = []
-    AI_NAMES = ['云师','jimmy仔','修师','范师','西米','铁锤','Zakk','胡哥','六爷','莎翁',]
-    random.shuffle(AI_NAMES)
-    result = AI_NAMES[:num]
-    result = list(map(lambda x: Player(name=x), result))
-    result.append(PLAYER)
-    random.shuffle(result)
-    PLAYERS = result
+        self.MakeUpAI(n_AI, cash)
+        self.PLAYERS = self.AI + [self.PLAYER]
+        random.shuffle(self.PLAYERS)
 
-    # init trash talk phase
-    for p in PLAYERS:
-        q = random.random()
-        opponent = ChooseOpponent(p)
-        if q > 0.7:
-            p.Talk('trash',p=opponent)
+        # init trash talk phase
+        for p in self.PLAYERS:
+            q = random.random()
+            opponent = p.ChooseOpponent()
+            if q > 0.7:
+                p.Talk('trash',p=opponent)
 
-    return PLAYERS
+    def MakeUpAI(self, n_AI, cash):
+        '''
+        0 - SB
+        1 - BB
+        2 - UTG
+        3 - UTG + 1
+        4 - CO
+        5 - BT
+        '''
+        self.AI = []
+        AI_NAMES = ['云师','jimmy仔','修师','范师','西米','铁锤','Zakk','胡哥','六爷','莎翁',]
+        random.shuffle(AI_NAMES)
+        result = AI_NAMES[:n_AI]
+        result = list(map(lambda x: Player(name=x, cash=cash), result))
+        random.shuffle(result)
+        self.AI = result
+    
+    def Shuffle(self):
+        num_list = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+        suit_list = ['s', 'h', 'c', 'd']
+        RawCards = [x + y for x in num_list for y in suit_list]
 
+        # once is enough yeah true but
+        random.shuffle(RawCards)
+        random.shuffle(RawCards)
+        random.shuffle(RawCards)
+        return RawCards
+    
+    def Preflop(self):
+        if not self.PREFLOP_FLAG:
+            for i in range(len(self.PLAYERS)):
+                self.PLAYERS[i].Draw()
+            self.PREFLOP_FLAG = True
+            return
+        else:
+            print('already PREFLOP!')
+
+    def Flop(self):
+        if not self.FLOP_FLAG:
+            self.TABLE.append(self.RawCards.pop())
+            self.TABLE.append(self.RawCards.pop())
+            self.TABLE.append(self.RawCards.pop())
+
+            ShowHand(self.TABLE)
+
+            for i in range(len(self.PLAYERS)):
+                self.PLAYERS[i].Combo()
+
+            self.FLOP_FLAG = True
+
+        else:
+            raise AlreadyFlopError('already FLOP!!')
+        print(f'current TABLE: {self.TABLE}\n')
+
+    def Turn(self):
+
+        if not self.TURN_FLAG:
+            self.TABLE.append(self.RawCards.pop())
+
+            ShowHand(self.TABLE)
+
+            for i in range(len(self.PLAYERS)):
+                self.PLAYERS[i].Combo()
+            
+            self.TURN_FLAG = True
+
+        else:
+            raise AlreadyTurnError('already TURN!!')
+        print(f'current TABLE: {self.TABLE}\n')
+
+    def River(self):
+
+        if not self.RIVER_FLAG:
+            self.TABLE.append(self.RawCards.pop())
+
+            ShowHand(self.TABLE)
+
+            for i in range(len(self.PLAYERS)):
+                self.PLAYERS[i].Combo()
+
+            self.RIVER_FLAG = True
+            return self.TABLE
+        else:
+            raise AlreadyRiverError('already RIVER!!')
+        print(f'current TABLE: {TABLE}\n')
 
 def ShowHand(slashstring) -> Table:
     '''
@@ -159,131 +243,32 @@ def ShowHand(slashstring) -> Table:
     return(Table(string[:-1]))
 
 
-def InitGame():
-
-    global TABLE
-    TABLE = []
-
-    global PREFLOP_FLAG
-    PREFLOP_FLAG = False
-
-    global FLOP_FLAG
-    FLOP_FLAG = False
-
-    global TURN_FLAG
-    TURN_FLAG = False
-
-    global RIVER_FLAG
-    RIVER_FLAG = False
-
-    global POOL
-    POOL = Pool()
-
-    global SB
-    SB = 5
-
-    global PLAYER
-    PLAYER = Player(AI=False)
-
-    global RawCards
-    RawCards = Shuffle()
-
-    MakeUpPlayers()
-
-
-def Preflop():
-    global PREFLOP_FLAG
-    global PLAYERS
-    if not PREFLOP_FLAG:
-        for i in range(len(PLAYERS)):
-            PLAYERS[i].Draw()
-        PREFLOP_FLAG = True
-        return
-    else:
-        print('already PREFLOP!')
-
-
-def Flop():
-    global FLOP_FLAG
-    global TABLE
-    if not FLOP_FLAG:
-        TABLE.append(RawCards.pop())
-        TABLE.append(RawCards.pop())
-        TABLE.append(RawCards.pop())
-
-        ShowHand(TABLE)
-
-        for i in range(len(PLAYERS)):
-            PLAYERS[i].Combo()
-
-        FLOP_FLAG = True
-        return TABLE
-    else:
-        raise AlreadyFlopError('already FLOP!!')
-    print(f'current TABLE: {TABLE}')
-
-
-def Turn():
-    global TURN_FLAG
-    global TABLE
-    if not TURN_FLAG:
-        TABLE.append(RawCards.pop())
-
-        ShowHand(TABLE)
-
-        for i in range(len(PLAYERS)):
-            PLAYERS[i].Combo()
-        
-        TURN_FLAG = True
-        return TABLE
-    else:
-        raise AlreadyTurnError('already TURN!!')
-    print(f'current TABLE: {TABLE}')
-
-
-def River():
-    global RIVER_FLAG
-    global TABLE
-    if not RIVER_FLAG:
-        TABLE.append(RawCards.pop())
-
-        ShowHand(TABLE)
-
-        for i in range(len(PLAYERS)):
-            PLAYERS[i].Combo()
-
-        RIVER_FLAG = True
-        return TABLE
-    else:
-        raise AlreadyRiverError('already RIVER!!')
-    print(f'current TABLE: {TABLE}')
-
-
 def main():
-    InitGame()
+
+    game = Game()
 
     global POOL
     # check SB
     print()
-    if PLAYERS[0].AI:
-        POOL = PLAYERS[0].Bet(SB)
+    if game.PLAYERS[0].AI:
+        POOL = game.PLAYERS[0].Bet(game.SB)
     else:   
-        POOL = PLAYER.Bet(SB)
+        POOL = game.PLAYER.Bet(game.SB)
 
     # check BB
-    if PLAYERS[1].AI:
-        POOL = PLAYERS[1].Bet(SB*2)
+    if game.PLAYERS[1].AI:
+        POOL = game.PLAYERS[1].Bet(game.SB*2)
     else:   
-        POOL = PLAYER.Bet(SB*2)
+        POOL = game.PLAYER.Bet(game.SB*2)
 
     # Preflop
-    Preflop()
+    game.Preflop()
     for i in range(2, 6):
-        PLAYERS[i].Decide()
-    print(f'你的手牌：{PLAYER.ShowHand()}',)
+        game.PLAYERS[i].Decide()
+    print(f'你的手牌：{game.PLAYER.ShowHand()}\n',)
 
     # Flop
-    Flop()
+    game.Flop()
     for i in range(0, 6):
-        PLAYERS[i].Decide()
-    print(f'你的手牌：{PLAYER.ShowHand()}',)
+        game.PLAYERS[i].Decide()
+    print(f'你的手牌：{game.PLAYER.ShowHand()}\n',)
--- utils.py
+++ utils.py
@@ -1,27 +1,9 @@
-import random
-
-
-DEBUG = False
-
-
-def Shuffle():
-    global RawCards
-    num_list = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
-    suit_list = ['s', 'h', 'c', 'd']
-    RawCards = [x + y for x in num_list for y in suit_list]
-    # once is enough yeah but
-    random.shuffle(RawCards)
-    random.shuffle(RawCards)
-    random.shuffle(RawCards)
-    return RawCards
-
-
 CORPUS = {
-    'FOLD':['弃牌','🈚️了','没我了','扔掉','白白👋🏻','再见',],
+    'FOLD':['弃牌','🈚️了','没我了','扔掉','白白👋🏻','再见','无我了','这局没我','你们玩','我看戏','看你们表演',],
     'CHECK':['过','敲敲','叩叩','nb，过了','先过吧','我过',],
     'CALL':['跟','跟上','跟起来','我跟','跟他妈的','跟了啊',],
     'RAISE':['加','加他妈的','加起来','加啊',],
-    'ALLIN':['全押！','ALLIN！！','推他妈的！！','就这些了啊！！','我数数啊',],
+    'ALLIN':['全押！','ALLIN！！','推他妈的！！','就这些了啊！！','我数数啊','我看看谁不服哈','我小红牌呢',],
     'SMALLTALK':['哈哈','nb','日了','牌不行','荷官给点力啊','这啥牌','我吐了','我吐了呀','我服了','我他妈吐了','我他妈吐了啊',],
-    'TRASHTALK':['加油啊','能行吗','放松点','没关系，输就输了','敢玩吗','还能玩动吗',]
+    'TRASHTALK':['加油啊','能行吗','放松点','没关系，输就输了呀','敢玩吗','还能玩动吗',]
 }
